77be76ff6097c02345c6a65f91db5e04a7cdda46
Pavel Glavatskikh <byba1100@mail.ru>
2023-09-19 22:57:09 +0400

    error correction, add Impl to class name

--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,26 +1,11 @@
 package web.service;
 
-import org.springframework.stereotype.Service;
 import web.model.Car;
-import java.util.ArrayList;
 import java.util.List;
 
-@Service
-public class CarService implements ICarService{
+public interface CarService {
 
-    @Override
-    public List<Car> createListCar() {
-        List<Car> cars = new ArrayList<>();
-        cars.add(new Car("BMV", 2003, 210));
-        cars.add(new Car("Honda", 2006, 220));
-        cars.add(new Car("Subaru", 2010, 290));
-        cars.add(new Car("Mitsubishi.", 2000, 200));
-        cars.add(new Car("Mazda", 1998, 190));
-        return cars;
-    }
+    public List<Car> createListCar();
 
-    @Override
-    public List<Car> getCars(List<Car> cars, Integer count) {
-        return cars.stream().limit(count).toList();
-    }
+    public List<Car> getCars(List<Car> cars, Integer count);
 }
---
+++ src/main/java/web/service/CarServiceImpl.java
@@ -0,0 +1,26 @@
+package web.service;
+
+import org.springframework.stereotype.Service;
+import web.model.Car;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CarServiceImpl implements CarService {
+
+    @Override
+    public List<Car> createListCar() {
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car("BMV", 2003, 210));
+        cars.add(new Car("Honda", 2006, 220));
+        cars.add(new Car("Subaru", 2010, 290));
+        cars.add(new Car("Mitsubishi", 2000, 200));
+        cars.add(new Car("Mazda", 1998, 190));
+        return cars;
+    }
+
+    @Override
+    public List<Car> getCars(List<Car> cars, Integer count) {
+        return cars.stream().limit(count).toList();
+    }
+}
